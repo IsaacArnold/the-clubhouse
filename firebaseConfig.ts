edcb0dda45bea9e7bac458b9f12231698aa58e5f
Isaac Arnold <isaacarnold@icloud.com>
2023-01-21 22:45:23 +1000

    Install Firebase + ChakraUI

---
+++ firebaseConfig.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwespNKizK3egdGzrV3SBzFS4hFpC7sqk",
+  authDomain: "the-clubroom-9f94f.firebaseapp.com",
+  projectId: "the-clubroom-9f94f",
+  storageBucket: "the-clubroom-9f94f.appspot.com",
+  messagingSenderId: "471474574796",
+  appId: "1:471474574796:web:6ed33de8f599da222eb969",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
