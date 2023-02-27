33dac1230f9eb4d285ff2581250baf8d6aa857bd
Isaac Arnold <isaacarnold@icloud.com>
2023-02-27 11:58:09 +1000

    Adding Round configuration options

--- src/pages/RoundConfigure.tsx
+++ src/pages/RoundConfigure.tsx
@@ -1,13 +1,20 @@
 import { getAuth } from "firebase/auth";
-import { Spinner } from "@chakra-ui/react";
+import { Spinner, Input, Select } from "@chakra-ui/react";
 import router from "next/router";
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { initFirebase, database } from "firebaseConfig";
-import { collection, getDocs, query, DocumentData } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  query,
+  DocumentData,
+  addDoc,
+  where,
+} from "firebase/firestore";
 import { useEffect, useState, SetStateAction } from "react";
-import RoundName from "@/components/inputs/roundName";
+import moment from "moment";
 
 //#region --- Page styles ---
 const Contanier = styled.section`
@@ -39,17 +46,38 @@ const SignoutDiv = styled.div`
   align-items: center;
   margin: 0 auto;
 `;
+
+const Button = styled.button`
+  color: var(--primaryGreen);
+  font-weight: 700;
+  text-align: left;
+`;
 //#endregion
 
 const RoundConfigure = () => {
   const [courseNames, setCourseNames] = useState<DocumentData[]>([]);
+  const [roundName, setRoundName] = useState("");
+  const [courseID, setCourseID] = useState("");
+  const [roundDate, setRoundDate] = useState("");
+  const [userID, setUserID] = useState("");
+
   initFirebase();
   const auth = getAuth();
   const [user, loading] = useAuthState(auth);
 
   useEffect(() => {
-    const golfCourses = query(collection(database, "courses"));
+    setRoundDate(moment().format("dddd, MMMM D, YYYY"));
+
+    const getDBUserID = async () => {
+      const userRef = collection(database, "users");
+      const q = query(userRef, where("id", "==", user?.uid));
+      const snapshot = await getDocs(q);
+      const internalUserID = snapshot.docs[0].data().id;
+      setUserID(internalUserID);
+    };
+
     const getGolfCourses = async () => {
+      const golfCourses = query(collection(database, "courses"));
       const snapShot = await getDocs(golfCourses);
       const data: SetStateAction<DocumentData[]> = [];
       snapShot.forEach((doc) => {
@@ -57,10 +85,10 @@ const RoundConfigure = () => {
       });
       setCourseNames(data);
     };
+
+    getDBUserID();
     getGolfCourses();
   }, []);
-
-  console.log(courseNames);
 
   if (loading) {
     return <Spinner color="blue.500" />;
@@ -77,8 +105,22 @@ const RoundConfigure = () => {
     );
   }
 
+  const onRoundConfigSubmit = async () => {
+    const collectionRef = collection(database, "rounds");
+    const docRef = await addDoc(collectionRef, {
+      roundName,
+      courseID: courseID,
+      roundDate: roundDate,
+      userID: userID,
+    });
+    // Resets the input fields
+    setRoundName("");
+  };
+
   /*
-  To configure the round we would create a document under Rounds with the user inputted roundID. We would then fill out the fields in that doc with the courseID, based on the selected course and the date
+  To configure the round we would create a document under Rounds with the user specified roundID. We would then fill out the fields in that doc with the courseID, based on the selected course and the date.
+
+  To assign the round to a specific user, we assign the Auth userID to the userID in the Firestore doc.
   */
 
   return (
@@ -86,12 +128,33 @@ const RoundConfigure = () => {
       <Contanier>
         <h1>Round Details</h1>
         <InternalContent>
-          <h3>Choose a course</h3>
-          {courseNames.map((course) => (
-            <p key={course.courseID}>{course.courseName}</p>
-          ))}
-          <RoundName />
-          <button>
+          <h4>Choose a course</h4>
+          <Select
+            placeholder="Select course"
+            isRequired={true}
+            onChange={(e) => setCourseID(e.target.value)}
+          >
+            {courseNames.map((course) => (
+              <option value={course.courseID} key={course.courseID}>
+                {course.courseName}
+              </option>
+            ))}
+          </Select>
+
+          <Contanier>
+            <label id="roundName">Round name</label>
+            <Input
+              mb={4}
+              focusBorderColor="#4B9D6F"
+              id="roundName"
+              type="text"
+              placeholder="wynnum0502"
+              value={roundName}
+              onChange={(e) => setRoundName(e.target.value)}
+            />
+          </Contanier>
+
+          <button onClick={onRoundConfigSubmit}>
             <Link href="/scorecard/Scorecard">Start</Link>
           </button>
         </InternalContent>
