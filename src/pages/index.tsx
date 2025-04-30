3aa63b7d56e4cee046ef881c9a29db8be805d2b2
Isaac Arnold <67135844+IsaacArnold@users.noreply.github.com>
2025-04-30 20:06:02 +1000

    Remove zoom on Safari for inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta charSet='utf-8' />
         <title>The Clubhouse</title>
         <meta name='description' content='The Clubhouse - Track your golf game' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
       </Head>
       <main>
         <SignIn />
